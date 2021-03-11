50933eb48aae65ab0d853d69c2a84b5b2ab85c8c
Victor Bruno Paro <victorbparo@gmail.com>
2021-03-10 21:36:57 -0300

    Acertando as configurações no index.js pra funcionar no host online

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
 
+const app = express();
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const { testMessage, commandRoutes, messageRoutes } = require('./routes/basic/mainRoute.js');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.telegram.setWebhook(`${process.env.BOT_URL}/bot${process.env.BOT_TOKEN}`);
+app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
 
 bot.use(async (ctx, next) => {
     const start = new Date();
@@ -18,3 +24,7 @@ testMessage(bot);
 
 
 bot.launch();
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
